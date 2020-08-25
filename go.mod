3490ae5d0f4b10077e95b597bbf23770c586c1e0
justnat3 <nd2010@reedusa.com>
2020-08-24 22:42:40 -0500

    first commit :sparkles:

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/justnat3/goAPI
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	a := 5
+	b := 6
+	summ := sum(a, b)
+	println(summ)
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
