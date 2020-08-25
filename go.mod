f2151e4a973b18992f8e4df81f5b638bb7ea2e3c
justnat3 <nd2010@reedusa.com>
2020-08-24 23:05:50 -0500

    rolls

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/justnat3/goAPI
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
-func main() {
-	a := 5
-	b := 6
-	summ := sum(a, b)
-	println(summ)
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//Roll is model for sushi
+type Roll struct {
+	ID          string `json:"id"`
+	ImageNumber string `json:"imageNumber"`
+	Name        string `json:"name"`
+	Ingredients string `json:"ingredients"`
 }
 
-func sum(a int, b int) int {
-	return a + b
+//init rolls var as a slice
+var rolls []Roll
+
+func getRolls(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(rolls)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/sushi", getRolls).Methods("GET")
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
